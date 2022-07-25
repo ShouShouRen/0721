b2ceea5882926ceb72a85db1d116abad4f5adb4d
Peter Chen <peterchen2017125@gmail.com>
2022-07-25 10:14:15 +0800

    20220725

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 new WOW().init();
 
 $(document).ready(function () {
-    // $(window).scroll(function () {
-    //     if ($(window).scrollTop() > 200) {
-    //         $("nav").addClass("round11");
-    //         $("nav").addClass("theme-bg-color");
-    //     } else {
-    //         $("nav").removeClass("round11");
-    //         $("nav").removeClass("theme-bg-color");
-    //         $("nav").removeClass("bg-filter");
-    //     }
-    // });
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 200) {
+            // $("nav").addClass("round11");
+            $("nav").addClass("theme-bg-color");
+        } else {
+            // $("nav").removeClass("round11");
+            $("nav").removeClass("theme-bg-color");
+            $("nav").removeClass("bg-filter");
+        }
+    });
     $(window).scroll(function () {
         if ($(window).width() < 768) {
             $("#navbarNav a").click(function () {
